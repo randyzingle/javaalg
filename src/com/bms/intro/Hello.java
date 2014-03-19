39da043ee5b4df8a6066a5a23ca7d4e353cfa306
randyzingle <randy.zingle@gmail.com>
2014-03-19 17:08:56 -0400

    added hello page

---
+++ src/com/bms/intro/Hello.java
@@ -0,0 +1,9 @@
+package com.bms.intro;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello and welcome to my project");
+		System.out.println("this project contains introductory java algorithms");
+	}
+}
