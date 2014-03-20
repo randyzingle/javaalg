055c43cf17789396b67b4456df9112617641b797
randyzingle <randy.zingle@gmail.com>
2014-03-20 16:43:21 -0400

    added assests

---
+++ src/com/bms/io/FIleStreams1.java
@@ -0,0 +1,13 @@
+package com.bms.io;
+
+public class FIleStreams1 {
+	
+	private String BASE_PATH = "/home/rzingle/work/projects/java/javaalg/src/assets";
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
