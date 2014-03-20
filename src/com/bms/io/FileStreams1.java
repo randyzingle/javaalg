1b187a834cdb61fd69971bd82707f1fb0fe19273
Randy Zingle <rzingle@wgen.net>
2014-03-20 16:56:08 -0400

    renamed filestream

---
+++ src/com/bms/io/FileStreams1.java
@@ -0,0 +1,19 @@
+package com.bms.io;
+
+public class FileStreams1 {
+	
+	private String BASE_PATH = "/home/rzingle/work/projects/java/javaalg/src/assets/";
+	
+	public static void main(String[] args) {
+		new FileStreams1().run();
+	}
+	
+	private void run() {
+		try {
+			// do stuff
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
