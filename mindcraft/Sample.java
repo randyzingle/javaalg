8d5d870a93433d516bf85a406098a7426738ae08
Alvores <Gabrielzingle@hotmail.com>
2014-03-31 19:53:18 -0400

    Update Sample.java

--- mindcraft/Sample.java
+++ mindcraft/Sample.java
@@ -1,5 +1,8 @@
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello from the Mindcraft Project and Gabe");
+		for (int i=0; i<10; i++) {
+			System.out.println(i);
+		}
 	}
 }
