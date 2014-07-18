662492670697eb4752bea18cfacea7195ba7a10e
randyzingle <randy.zingle@gmail.com>
2014-07-18 15:11:18 -0400

    a

--- algorithms/src/com/bms/intro/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.bms.intro;
-
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.println("hello and welcome to my project");
-		System.out.println("this project contains introductory java algorithms");
-	}
-}
