f1e5664b12fb56dffb1ea3fe8ec8d73ef48b1986
gabrielfinnrzingle <Gabrielzingle@hotmail.com>
2014-11-01 16:47:56 -0400

    blha

--- alvores/src/algorithms/Exercise1_1_19.java
+++ alvores/src/algorithms/Exercise1_1_19.java
@@ -9,9 +9,14 @@ public class Exercise1_1_19 {
 		}
 	
 	public static void main(String[] args) {
-		for (int N = 0; N < 100; N++) {
+		long startTime = System.currentTimeMillis();
+		int nloop = 40;
+		for (int N = 0; N < nloop; N++) {
 			System.out.println(N + " " + F(N));
 		}
+		long endTime = System.currentTimeMillis();
+		long totalTime = (endTime - startTime);
+		System.out.printf("n=%d, time=%d%n", nloop, totalTime);
 
 	}
 
