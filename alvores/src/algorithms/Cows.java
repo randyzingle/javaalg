dc48da4479ae3ef0c34349cac0dc84b3d62d75ab
gabrielfinnrzingle <Gabrielzingle@hotmail.com>
2015-01-06 17:45:23 -0500

    file

---
+++ alvores/src/algorithms/Cows.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+public class Cows {
+	double position;
+	String name;
+	int numCows;
+	//getPosition (returns Point2D.Double)
+	//setPosition (give it a Point2D.Double)
+
+	//get/set name (ex. Bessie)
+	//get/set number (a simple integer associated with the cow)
+	public double getPosition() {
+		return position;
+		
+	}
+	public  void setPosition() {
+		//position = ;
+	}
+	
+}
