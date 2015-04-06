85021503b4929f3669047b227d1068956770842b
randyzingle <randy.zingle@gmail.com>
2015-04-06 10:09:05 -0400

    runner

--- raf/dungeons/src/com/bms/dnd/DungeonRunner.java
+++ raf/dungeons/src/com/bms/dnd/DungeonRunner.java
@@ -3,11 +3,29 @@ package com.bms.dnd;
 public class DungeonRunner {
 
 	public static void main(String[] args) {
+
+		DungeonRunner dr = new DungeonRunner();
+//		dr.makeCharacter();
+		dr.testDice();
+
+	}
+	
+	public void makeCharacter() {
 		Character c = new Character();
 //		c.setName("Baldur");
 		c.setCharisma(20);
 		System.out.println(c);
-
+	}
+	
+	public void testDice() {
+		int max = 0;
+		int min = Integer.MAX_VALUE;
+		for (int i=0; i<100; i++) {
+			int n = DndUtils.rollDice(6, 3);
+			if (n > max) max = n;
+			System.out.println(n);
+		}
+		System.out.println("max: " + max);
 	}
 
 }
